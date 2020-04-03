957debf0eb407d7fbcec312c85bd6077ff86743c
Oscar Perera <oscarperera99@gmail.com>
2020-04-02 20:03:46 -0600

    segundo commit

---
+++ force-app/main/default/lwc/app/app.js
@@ -0,0 +1,91 @@
+import { LightningElement, track, api } from 'lwc';
+import createContactRecord from '@salesforce/apex/ContactController.createContactRecord';
+
+//Comienza sección que involucra a la inserción del objeto
+
+
+
+
+
+
+export default class App extends LightningElement {
+
+    @track nombre = '';
+    @track apellido = '';
+    @track correo = '';
+
+    actualizaNombre(event){
+        this.nombre = event.target.value;
+    }
+    actualizaApellido(event){
+        this.apellido = event.target.value;
+    }
+    actualizaCorreo(event){
+        this.correo = event.target.value;
+    }
+    
+    insertarContacto(){
+    
+        let cont = { 'sobjectType': 'Contact' };
+        cont.FirstName = this.nombre;
+        cont.LastName = this.apellido;
+        cont.Email = this.correo;
+
+        createContactRecord({newRecord: cont})
+        .then(result => {
+            this.recordId = result;
+            console.log(result);
+        })
+        .catch(error => {
+            console.log(error);
+            this.error = error;
+        });
+
+    }
+//Finaliza Sección
+@track value = [''];
+@track cantidadComidas= 0;
+@track money = 0;
+   get options() {
+        return [
+            { label: 'Facebook', value: 'option1' },
+            { label: 'Twitter', value: 'option2' },
+        ];
+    }
+
+    get selectedValues() {
+        return this.value.join(',');
+    }
+
+    handleChange(e) {
+        this.value = e.detail.value;
+    }
+
+       comidasSeleccionadas(event) {
+       
+        const buttonNumber = event.target.dataset.buttonNumber;
+        this.cantidadComidas = event.target.value;
+        this.money = 0;
+    }
+
+    equivalenciaDolar(event) {
+      
+    this.money = event.target.value*this.cantidadComidas;
+
+    }
+
+    equivalenciaPeso(event) {
+      
+    this.money = event.target.value*this.cantidadComidas;
+
+    }
+
+
+ icono = {
+        picture: 'direccion de logo'
+    };
+
+
+
+
+}
