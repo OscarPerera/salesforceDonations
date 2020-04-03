9dfe34d3b506712af7b060f21201f3d8938076b3
Oscar Perera <oscarperera99@gmail.com>
2020-04-03 04:20:43 -0600

    aaa

--- force-app/main/default/lwc/app/app.js
+++ force-app/main/default/lwc/app/app.js
@@ -1,10 +1,6 @@
 import { LightningElement, track, api } from 'lwc';
 import createContactRecord from '@salesforce/apex/ContactController.createContactRecord';
 
-//Comienza sección que involucra a la inserción del objeto
-
-
-
 
 
 
@@ -13,6 +9,7 @@ export default class App extends LightningElement {
     @track nombre = '';
     @track apellido = '';
     @track correo = '';
+    
 
     actualizaNombre(event){
         this.nombre = event.target.value;
@@ -42,10 +39,44 @@ export default class App extends LightningElement {
         });
 
     }
-//Finaliza Sección
+
+
+
+
+VALOR_COMIDA=50;
 @track value = [''];
 @track cantidadComidas= 0;
 @track money = 0;
+date = Date.now();
+nameOpportunity = "nueva oportunidad"
+
+    insertarOpportunity(){
+        let opot = {'sobjectType': 'Opportunity'};
+        opot.Name = this.nameOpportunity;
+        opot.CloseDate = this.date;
+        opot.Amount = this.cantidadComidas*VALOR_COMIDA;
+
+        createOpportunityRecord({newRecord: opot})
+        .then(result => {
+            this.recordId = result;
+            console.log(result);
+        })
+        .catch(error => {
+            console.log(error);
+            this.error = error;
+        });
+
+
+    }
+
+
+    dobleInsercion(){
+
+        this.insertarContacto();
+        this.insertarOpportunity();
+        
+    }
+
    get options() {
         return [
             { label: 'Facebook', value: 'option1' },
